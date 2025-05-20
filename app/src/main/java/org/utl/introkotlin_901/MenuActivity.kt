81544b6ad7b4724bebb4d2e87d6184dc4b239274
CharlyPadilla <charlypadia90888@gmail.com>
2025-05-19 20:37:52 -0600

    Antes de regresar a cambios cuando si funcionaba

---
+++ app/src/main/java/org/utl/introkotlin_901/MenuActivity.kt
@@ -0,0 +1,35 @@
+package org.utl.introkotlin_901
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.utl.introkotlin_901.ejemplo1.Operaciones
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+        val btnSaludo = findViewById<Button>(R.id.btn_saludar)
+
+        btnSaludo.setOnClickListener { saludarNavegante() }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun saludarNavegante(){
+        val intent = Intent(this, Operaciones::class.java)
+        startActivity(intent)
+
+    }
+
+}
